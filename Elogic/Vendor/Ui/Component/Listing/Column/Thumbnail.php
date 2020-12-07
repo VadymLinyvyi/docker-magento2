449a437d148fce0716227c9ab3081f40f102e6fb
VadymLinyvyi <raven.lazy@gmail.com>
2020-12-07 19:35:15 +0200

    added image to ui grid

---
+++ Elogic/Vendor/Ui/Component/Listing/Column/Thumbnail.php
@@ -0,0 +1,89 @@
+<?php
+namespace Elogic\Vendor\Ui\Component\Listing\Column;
+
+use Magento\Catalog\Helper\Image;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class Thumbnail
+ * @package Elogic\Vendor\Ui\Component\Listing\Column
+ */
+class Thumbnail extends Column
+{
+    /**
+     * @var StoreManagerInterface $storeManager
+     */
+    protected $storeManager;
+    /**
+     * @var Image
+     */
+    private $imageHelper;
+
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        Image $imageHelper,
+        UrlInterface $urlBuilder,
+        StoreManagerInterface $storeManager,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->imageHelper = $imageHelper;
+        $this->urlBuilder = $urlBuilder;
+        $this->storeManager = $storeManager;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            $fieldName = $this->getData('name');
+            foreach ($dataSource['data']['items'] as & $item) {
+                $url = '';
+                if ($imageName = $item[$fieldName]) {
+                    $firstName = substr($imageName, 0, 1);
+                    $secondName = substr($imageName, 1, 1);
+                    $url = $this->storeManager->getStore()->getBaseUrl(
+                        \Magento\Framework\UrlInterface::URL_TYPE_MEDIA
+                    ).'logo/image/'.$firstName.'/'.$secondName.'/'.$imageName;
+                }
+                $item[$fieldName . '_src'] = $url;
+                $item[$fieldName . '_alt'] = $this->getAlt($item) ?: '';
+                $item[$fieldName . '_link'] = $this->urlBuilder->getUrl(
+                    'vendors/vendorslist/edit',
+                    ['id' => $item['entity_id']]
+                );
+                $item[$fieldName . '_orig_src'] = $url;
+            }
+        }
+
+        return $dataSource;
+    }
+
+    /**
+     * @param array $row
+     *
+     * @return null|string
+     */
+    protected function getAlt($row)
+    {
+        $altField = $this->getData('config/altField') ?: self::ALT_FIELD;
+        return isset($row[$altField]) ? $row[$altField] : null;
+    }
+}
